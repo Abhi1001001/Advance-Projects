b1c6e122c3b53a66e20c80ba3c218365af56c187
Abhi1001001 <av159593@gmail.com>
2024-08-15 13:32:04 +0530

    add initial file

--- abhishek-portfolio-SPA/src/Home.jsx
+++ abhishek-portfolio-SPA/src/Home.jsx
@@ -3,13 +3,16 @@ import React from "react";
 export default function Home() {
   return (
     <>
-      <div className="m-3">
-        <div className="card main-home text-start">
-          <div className="card-header fs-2 ">Abhishek Vishawakarma</div>
-          <div className="card-body card-main-home ">
+    {/* edited */}
+      <div className=""> 
+        <div class="card main-home text-start">
+          <div class="card-header fs-2 ">Abhishek Vishawakarma</div>
+          <div class="card-body card-main-home ">
             <div className="sm-card-main-home col-12 col-sm-6">
-              <h1 className="card-title main-text-home">I'm a Front-end Developer</h1>
-              <p className="card-text card-text-home">
+              <h1 class="card-title main-text-home">
+                I'm a Front-end Developer
+              </h1>
+              <p class="card-text card-text-home">
                 I'm a dedicated software developer with a passion for creating
                 innovative digital solutions.I aim to contribute to a dynamic
                 team, drive innovation, and continuously enhance my expertise in
@@ -17,8 +20,8 @@ export default function Home() {
                 prioritize user experience and performance, while staying
                 up-to-date with the latest industry trends and best practices.
               </p>
-              <a href="#" className="btn btn-home">
-                <span className="material-symbols-outlined fs-2">exit_to_app</span>
+              <a href="#" class="btn btn-home">
+                <span class="material-symbols-outlined fs-2">exit_to_app</span>
               </a>
             </div>
             <div className="img-home col-12 col-sm-5 ">
@@ -35,4 +38,3 @@ export default function Home() {
     </>
   );
 }
-
